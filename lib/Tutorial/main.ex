f0b14cc856519d05159dee3c4391e3144a3e5151
Matthias Geissendoerfer <matthias.geissendoerfer@hermes-ws.com>
2017-08-14 17:24:41 +0200

    refactoring

--- lib/Tutorial/main.ex
+++ lib/Tutorial/main.ex
@@ -1,15 +1,19 @@
 defmodule Main do
   def start do
-    IO.puts Math.mult(4,6)
-    IO.puts Math.zero?(50)
-    IO.puts Math.zero?(0)
+    Main.show Math.mult(4,6)
+    Main.show Math.zero?(50)
+    Main.show Math.zero?(0)
 
-    IO.puts Concat.join("Hello", "world")
-    IO.puts Concat.join("Hello", "world", "-")
+    Main.show Concat.join("Hello", "world")
+    Main.show Concat.join("Hello", "world", "-")
 
-    Recursion.print_multiple_times("Moin",5)
-    IO.puts Math.sum_list([1,2,3,4],0)
+    Main.showAll Recursion.print_multiple_times("Moin",5)
+    Main.show Math.sum_list([1,2,3,4],0)
     Main.showAll(Math.double_each([1,2,3,4]))
+  end
+
+  def show(content) do
+    IO.puts(content)
   end
 
   def showAll([head| tail]) do
